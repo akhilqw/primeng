387b015183a101f189e85cb199d0f011da1e4383
akhil <g@dddd.com>
2025-02-01 00:42:06 -0500

    ss

--- apps/showcase/app/main.ts
+++ apps/showcase/app/main.ts
@@ -2,4 +2,6 @@ import { AppComponent } from '@/components/layout/app.component';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app.config';
 
-bootstrapApplication(AppComponent, appConfig).catch((err) => console.error(err));
+bootstrapApplication(AppComponent, appConfig).catch((err) =>  
+    
+    console.error(err));
